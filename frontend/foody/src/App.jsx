9f0134a4c2cf663a1236e10abc4afcab7f849028
Shubham Kumar <128150904+Shubham1893@users.noreply.github.com>
2025-02-15 19:12:08 +0530

    Update App.jsx

--- frontend/foody/src/App.jsx
+++ frontend/foody/src/App.jsx
@@ -19,7 +19,7 @@ import EditRecipe from './Pages/EditRecipe'
 const getAllRecipes = async()=>{
   
   let allRecipes =[]
-  await axios.get("https://backend-3-a64y.onrender.com/recipe")
+  await axios.get("https://backend-3-2-f9p5.onrender.com/recipe")
   .then(res=>{
     allRecipes = res.data
   })
